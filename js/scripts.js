963be49677f2956d806a84dd82989f8bddece1be
taj-code <tajofisher4@gmail.com>
2021-08-20 15:58:48 -0700

    trying to get result/input results

--- js/scripts.js
+++ js/scripts.js
@@ -5,26 +5,24 @@ const numbers = ["1, 2, 3, 4, 5, 6, 7, 8, 9, 10"];
 
  function inputArray(input) {
    const array = []
-   for (i = 0; i <= input; i++)
-    array.push(i);
- }
- return array;
+   for (i = 0; i <= input; i++) {
+     array.push(i);
+   }
+  return array;
 }
-
-
 
 
 function numbers(input) {
-  if (input.includes("1")
-  return "Beep!"
+  if (input.includes("1")) {
+    return "Beep!!";
+  } else if (input.includes("2"));
+    return "Boop!!";
+  } else if (input.includes("3"));
 
-  2 
-  Boop!
-
-  3 
-  Won't you be my neighbor??
+  return "Won't you be my neighbor??"
 }
-
+  return input;
+}
 
 
 
