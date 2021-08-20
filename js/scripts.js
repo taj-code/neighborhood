3095c20db0edf8d261d611e6e61303468e7bc53f
taj-code <tajofisher4@gmail.com>
2021-08-20 14:26:31 -0700

    testing scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,19 @@
 const numbers = ["1, 2, 3, 4, 5, 6, 7, 8, 9, 10"];
+ let arrat = ["1, 2, 3, 4, 5, 6, 7, 8, 9, 10"]
 
-function firstNumber(number) {
-array.forEach(element => {
+ array.prototype.push = ["hi", "there"]
+
+
+
+// function firstNumber(number) {
+// array.forEach(element => {
   
-});
+// });
 
-}
+// }
 
-function words(word) {
-array.forEach(element)
+// function words(word) {
+// array.forEach(element)
 
 
-}
+// }
